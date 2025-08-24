d987a5576e65d54b2d35a3a2eded9da6bfe901e4
Your Name <youremail@example.com>
2025-08-24 12:35:15 +0600

    google login done

---
+++ src/component/googleLogin/GoogleLoginButton.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// src/features/auth/components/GoogleLoginButton.tsx
+import { GoogleLogin, type CredentialResponse } from "@react-oauth/google";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { setUser } from "@/features/auth/authSlice";
+
+const GoogleLoginButton = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = async (credentialResponse: CredentialResponse) => {
+    if (!credentialResponse.credential) {
+      toast.error("Google login failed: No credential found");
+      return;
+    }
+
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_API_BASE_URL}/api/auth/google-login`,
+        { token: credentialResponse.credential },
+        { withCredentials: true }
+      );
+
+      dispatch(setUser(res.data.user));
+      toast.success("Login successful with Google");
+      navigate("/");
+    } catch (err: any) {
+      console.error("Google login error:", err);
+      toast.error("Google login failed");
+    }
+  };
+
+  return (
+    <GoogleLogin
+      onSuccess={handleGoogleLogin}
+      onError={() => toast.error("Google login failed")}
+    />
+  );
+};
+
+export default GoogleLoginButton;

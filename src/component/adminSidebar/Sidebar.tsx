b2e52720bd106e11ecd6e48e829d0f48f74d6a4f
Your Name <youremail@example.com>
2025-08-28 23:18:11 +0600

    ready admin sidebar

---
+++ src/component/adminSidebar/Sidebar.tsx
@@ -0,0 +1,80 @@
+import { handleLogout } from "@/features/auth/authService";
+import React from "react";
+import { FaHome, FaUser, FaWallet, FaExchangeAlt, FaSignOutAlt, FaBackspace } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+interface SidebarProps {
+  role: "user" | "admin" | "agent";
+}
+
+
+
+const Sidebar: React.FC<SidebarProps> = ({ role }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+
+  const logout = () => handleLogout(dispatch, navigate);
+
+  return (
+    <aside className="w-64 bg-white shadow-lg flex flex-col">
+      <div className="p-6 text-2xl font-bold text-blue-600">Admin Panel</div>
+      <nav className="flex-1 px-4 space-y-4">
+        <Link
+          to="/admin"
+          className={`flex items-center gap-3 ${
+            location.pathname === "/admin" ? "text-blue-600 font-semibold" : "text-gray-700"
+          }`}
+        >
+          <FaHome /> Dashboard
+        </Link>
+
+        {role === "admin" && (
+          <>
+            <Link
+              to="/admin/userTable"
+              className={`flex items-center gap-3 ${
+                location.pathname === "/admin/userTable" ? "text-blue-600 font-semibold" : "text-gray-700"
+              }`}
+            >
+              <FaUser /> Users
+            </Link>
+            <Link
+              to="/admin/wallets"
+              className={`flex items-center gap-3 ${
+                location.pathname === "/admin/wallets" ? "text-blue-600 font-semibold" : "text-gray-700"
+              }`}
+            >
+              <FaWallet /> Wallets
+            </Link>
+            <Link
+              to="/admin/transactions"
+              className={`flex items-center gap-3 ${
+                location.pathname === "/admin/transactions" ? "text-blue-600 font-semibold" : "text-gray-700"
+              }`}
+            >
+              <FaExchangeAlt /> Transactions
+            </Link>
+            <Link
+              to="/"
+              className={`flex items-center gap-3 ${
+                location.pathname === "/" ? "text-blue-600 font-semibold" : "text-gray-700"
+              }`}
+            >
+              <FaBackspace /> Go back
+            </Link>
+          </>
+        )}
+      </nav>
+      <div className="p-4 border-t">
+        <button onClick={logout} className="flex items-center gap-3 text-red-500 hover:text-red-600 w-full">
+          <FaSignOutAlt /> Logout
+        </button>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
